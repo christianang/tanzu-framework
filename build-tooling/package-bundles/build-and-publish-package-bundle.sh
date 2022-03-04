d90051f45dd80234fadce2e4fbf9d03174a58588
Edwin Xie <exie@vmware.com>
2022-03-04 00:30:21 +0000

    Run make tools in docker build-tooling
    Co-authored-by: Christian Ang <angc@vmware.com>
    Co-authored-by: Edwin Xie <exie@vmware.com>

--- build-tooling/package-bundles/build-and-publish-package-bundle.sh
+++ build-tooling/package-bundles/build-and-publish-package-bundle.sh
@@ -6,5 +6,7 @@ set -euo pipefail
 docker login --username "${REGISTRY_USERNAME}" --password "${REGISTRY_PASSWORD}" "${REGISTRY_SERVER}"
 
 cd /tanzu-framework
+
+make -C ./hack/tools ytt kbld imgpkg
 make package-bundle
 OCI_REGISTRY="${REGISTRY_SERVER}" make push-package-bundle
